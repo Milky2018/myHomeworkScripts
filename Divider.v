da1d18a8537eb97f598ad0a0cbbf7a02e9f2bd8f
Milky2018 <37078809+Milky2018@users.noreply.github.com>
2018-10-08 23:47:29 +0800

    once

--- Divider.v
+++ Divider.v
@@ -13,14 +13,14 @@ my_div u_my_div(
 	.clk(clk),
 	.rst(rst),
    
-	.signed_div_i(sign),
-	.opdata1_i(x),
-	.opdata2_i(y),
-	.start_i(div),
-	.annul_i(1'b0),
+	.sign(sign),
+	.x(x),
+	.y(y),
+	.start(div),
+	.cancel(1'b0),
  
-	.result_o(result),
-	.ready_o(complete)
+	.result(result),
+	.complete(complete)
 );
 
 endmodule
--- my_div.v
+++ my_div.v
@@ -2,23 +2,19 @@
 `define DivByZero          2'b01
 `define DivOn              2'b10
 `define DivEnd             2'b11
-`define DivResultReady     1'b1
-`define DivResultNotReady  1'b0
-`define DivStart           1'b1
-`define DivStop            1'b0
 
 module my_div(
 	input  	          clk,
 	input  	          rst,
 
-	input             signed_div_i,
-	input      [31:0] opdata1_i,
-	input      [31:0] opdata2_i,
-	input             start_i,
-	input             annul_i,
+	input             sign,
+	input      [31:0] x,
+	input      [31:0] y,
+	input             start,
+	input             cancel,
 
-	output reg [63:0] result_o,
-	output reg        ready_o
+	output reg [63:0] result,
+	output reg        complete
 );
 
 	wire [32:0] div_temp;
@@ -27,42 +23,42 @@ module my_div(
 	reg [64:0] dividend;
 	reg [1 :0] state;
 	reg [31:0] divisor; 
-	reg [31:0] temp_op1;
-	reg [31:0] temp_op2;
+	reg [31:0] temp_x;
+	reg [31:0] temp_y;
 
 	assign div_temp = {1'b0, dividend[63:32]} - {1'b0, divisor};
 
 	always @ (posedge clk) begin
 		if (rst == 1'b1) begin
 			state    <= `DivFree;
-			ready_o  <= `DivResultNotReady;
-			result_o <= {32'd0,32'd0};
+			complete  <= 1'b0;
+			result <= {32'd0,32'd0};
 		end else begin
 			case (state)
 				`DivFree: begin 
-					if(start_i == `DivStart && annul_i == 1'b0) begin
-			  			if(opdata2_i == 32'd0) begin
+					if(start == 1'b1 && cancel == 1'b0) begin
+			  			if(y == 32'd0) begin
 			    			state <= `DivByZero; 
 			  			end else begin
 			    			state <= `DivOn; 
 			    			cnt <= 6'b000000;
-			    			if(signed_div_i == 1'b1 && opdata1_i[31] == 1'b1 ) begin
-			      				temp_op1 = ~opdata1_i + 1; 
+			    			if(sign == 1'b1 && x[31] == 1'b1 ) begin
+			      				temp_x = ~x + 1; 
 			    			end else begin
-			      				temp_op1 = opdata1_i;
+			      				temp_x = x;
 			    			end
-			    			if(signed_div_i == 1'b1 && opdata2_i[31] == 1'b1 ) begin
-			      				temp_op2 = ~opdata2_i + 1; 
+			    			if(sign == 1'b1 && y[31] == 1'b1 ) begin
+			      				temp_y = ~y + 1; 
 			    			end else begin
-			      				temp_op2 = opdata2_i;
+			      				temp_y = y;
 			    			end
 				    		dividend <= {32'd0,32'd0};
-				    		dividend[32:1] <= temp_op1;
-				    		divisor <= temp_op2;
+				    		dividend[32:1] <= temp_x;
+				    		divisor <= temp_y;
 			  			end
 					end else begin 
-			  			ready_o <= `DivResultNotReady;
-			  			result_o <= {32'd0,32'd0};
+			  			complete <= 1'b0;
+			  			result <= {32'd0,32'd0};
 					end
 				end
 				`DivByZero: begin 
@@ -70,7 +66,7 @@ module my_div(
 					state <= `DivEnd;		 		
 				end
 				`DivOn: begin 
-					if(annul_i == 1'b0) begin
+					if(cancel == 1'b0) begin
 			  			if(cnt != 6'b100000) begin 
 			    			if(div_temp[32] == 1'b1) begin 
 			          			dividend <= {dividend[63:0] , 1'b0};
@@ -79,10 +75,10 @@ module my_div(
 			    			end
 			    			cnt <= cnt + 1;
 			  			end else begin 
-			    			if((signed_div_i == 1'b1) && ((opdata1_i[31] ^ opdata2_i[31]) == 1'b1)) begin
+			    			if((sign == 1'b1) && ((x[31] ^ y[31]) == 1'b1)) begin
 			        			dividend[31:0] <= (~dividend[31:0] + 1);
 			    			end
-			    			if((signed_div_i == 1'b1) && ((opdata1_i[31] ^ dividend[64]) == 1'b1)) begin              
+			    			if((sign == 1'b1) && ((x[31] ^ dividend[64]) == 1'b1)) begin              
 			        			dividend[64:33] <= (~dividend[64:33] + 1);
 			    			end
 			    			state <= `DivEnd; 
@@ -93,12 +89,12 @@ module my_div(
 					end	
 				end
 				`DivEnd: begin 
-					result_o <= {dividend[64:33], dividend[31:0]};  
-					ready_o <= `DivResultReady;
-					if(start_i == `DivStop) begin
+					result <= {dividend[64:33], dividend[31:0]};  
+					complete <= 1'b1;
+					if(start == 1'b0) begin
 			 	 		state <= `DivFree;
-			  			ready_o <= `DivResultNotReady;
-			  			result_o <= {32'd0,32'd0};       	
+			  			complete <= 1'b0;
+			  			result <= {32'd0,32'd0}; 
 					end		  	
 				end
 			endcase
